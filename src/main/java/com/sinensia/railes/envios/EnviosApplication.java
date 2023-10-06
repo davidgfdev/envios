e3357aefb40205000fc03a35b7c47b6b63616dff
unknown <davidgf.dev@gmail.com>
2023-10-06 12:22:49 +0200

    main configuration

--- src/main/java/com/sinensia/railes/envios/EnviosApplication.java
+++ src/main/java/com/sinensia/railes/envios/EnviosApplication.java
@@ -2,10 +2,14 @@ package com.sinensia.railes.envios;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.web.client.RestTemplate;
 
-@SpringBootApplication
+@EntityScan(basePackages = "com.sinensia.railes.model")
+@EnableJpaRepositories(basePackages = "com.sinensia.railes.dao")
+@SpringBootApplication(scanBasePackages = {"com.sinensia.railes.controller", "com.sinensia.railes.service"})
 public class EnviosApplication {
 
 	public static void main(String[] args) {
